d23ad6a556272d2e75e406eceb23817ee9592bd6
chriz <josechris042@gmail.com>
2026-03-13 12:46:25 +0400

    hellwal and shi finalized

--- fish/functions/clock.fish
+++ fish/functions/clock.fish
@@ -1,3 +1,3 @@
-function clock --wraps='tty-clock -c -C 2 -D -t' --description 'alias clock tty-clock -c -C 2 -D -t'
-    tty-clock -c -C 2 -D -t $argv
+function clock --wraps='tty-clock -c -C 6 -D -t' --description 'alias clock tty-clock -c -C 6 -D -t'
+    tty-clock -c -C 6 -D -t $argv
 end
